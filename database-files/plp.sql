12e7eb901567bc94efa2b8d9cf79a7ed6b10f3ae
Alex <alexangione419@gmail.com>
2025-05-27 21:32:43 +0200

    initial ddl

--- database-files/plp.sql
+++ database-files/plp.sql
@@ -1,13 +1,16 @@
 DROP DATABASE IF EXISTS plp;
 CREATE DATABASE IF NOT EXISTS plp;
+
 USE plp;
+
 
 CREATE TABLE IF NOT EXISTS political_party (
     partyID INT PRIMARY KEY,
     partyCohesionScore INT,
     partyName VARCHAR(255),
+    leaderID INT,
+    FOREIGN KEY (leaderID) REFERENCES mep(mepID)
 );
-
 
 CREATE TABLE IF NOT EXISTS mep (
     mepID INT PRIMARY KEY,
@@ -25,12 +28,45 @@ CREATE TABLE IF NOT EXISTS legislation (
     dateOfVote DATE,
 );
 
+CREATE TABLE IF NOT EXISTS referenceDocument(
+    legislationID INT,
+    referenceID INT,
+    PRIMARY KEY (legislationID, referenceID),
+    FOREIGN KEY (legislationID) REFERENCES legislation(legislationID),
+);
 
 CREATE TABLE IF NOT EXISTS vote (
     mepID INT,
     legislationID INT,
-    vote VARCHAR(255),
+    description VARCHAR(255),
     PRIMARY KEY (mepID, legislationID),
     FOREIGN KEY (mepID) REFERENCES mep(mepID),
     FOREIGN KEY (legislationID) REFERENCES legislation(legislationID)
 );
+
+CREATE TABLE IF NOT EXISTS watchList (
+    watchListID INT PRIMARY KEY,
+);
+
+CREATE TABLE IF NOT EXISTS user (
+    userID INT PRIMARY KEY,
+    age INT,
+    countryOfOrigin VARCHAR(255),
+    firstName VARCHAR(255),
+    lastName VARCHAR(255),
+    partyID INT,
+    watchListID INT,
+    FOREIGN KEY (partyID) REFERENCES political_party(partyID),
+    FOREIGN KEY (watchListID) REFERENCES watchList(watchListID)
+);
+
+
+CREATE TABLE IF NOT EXISTS mepToWatchList (
+    watchListID INT,
+    mepID INT,
+    PRIMARY KEY (watchListID, mepID),
+    FOREIGN KEY (watchListID) REFERENCES watchList(watchListID),
+    FOREIGN KEY (mepID) REFERENCES mep(mepID)
+);
+
+
